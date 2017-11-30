9677c42c456eb0f18215771cbcceeb7c9d243b26
khalid.cuet <khalid.cuet@gmail.com>
2017-11-30 18:17:23 -0500

    forLoop

--- webData/webData/Program.cs
+++ webData/webData/Program.cs
@@ -10,8 +10,15 @@ namespace webData
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("I am testing");
             Console.WriteLine("HMMMMMMMMMMMMMMMMMMMMMMMM");
+            for (int a = 10; a < 20; a = a + 1)
+            {
+                Console.WriteLine("value of a: {0}", a);
+            }
+            Console.ReadLine();
+        }
         }
     }
-}
+
